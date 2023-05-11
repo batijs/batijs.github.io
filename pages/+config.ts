f6cb4e6e6a17051aa63812ce5d53c8059d5523d7
Joël Charles <joel.charles91@gmail.com>
2023-05-11 15:49:44 +0200

    cleanup + prerender

--- pages/+config.ts
+++ pages/+config.ts
@@ -3,9 +3,10 @@ import Layout from "../layouts/LayoutDefault";
 
 // Default config (can be overriden by pages)
 export default {
+  prerender: true,
   Layout,
   // <title>
-  title: "My Vike App",
+  title: "Bâti",
   // <meta name="description">
-  description: "Demo showcasing Vike",
+  description: "Bâti",
 } satisfies Config;
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,13 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), solid()],
+  plugins: [
+    tsconfigPaths(),
+    solid({
+      vps: {
+        prerender: true,
+        disableAutoFullBuild: false,
+      },
+    }),
+  ],
 });
