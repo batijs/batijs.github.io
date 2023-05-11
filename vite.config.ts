610d42703a1823f0cf438f3b92c5700da9b9ec60
Joël Charles <joel.charles91@gmail.com>
2023-05-11 16:14:21 +0200

    add base URL

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: process.env.CI ? "/batijs.github.io/" : undefined,
   plugins: [
     tsconfigPaths(),
     solid({
