4cbcb0df33e109c58fddb1c9f25bbadf24aeab05
Joël Charles <joel.charles91@gmail.com>
2023-05-11 15:56:49 +0200

    add under construction header

--- layouts/LayoutDefault.tsx
+++ layouts/LayoutDefault.tsx
@@ -4,14 +4,21 @@ import type { JSX } from "solid-js";
 
 export default function LayoutDefault(props: { children?: JSX.Element }) {
   return (
-    <div class="flex max-w-5xl m-auto">
+    <div class="flex flex-col mx-auto">
+      <header class="w-full flex justify-center py-4 bg-warning">
+        <span class="text-4xl text-warning-content">⚠ Under construction</span>
+      </header>
       <Content>{props.children}</Content>
     </div>
   );
 }
 
 function Content(props: { children: JSX.Element }) {
-  return <div class="container min-h-screen">{props.children}</div>;
+  return (
+    <div class="container max-w-6xl mx-auto mt-6 min-h-screen">
+      {props.children}
+    </div>
+  );
 }
 
 function Logo() {
