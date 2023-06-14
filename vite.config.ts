5598847742d903d9c1802ebf0f38c822772a8390
Joël Charles <joel.charles91@gmail.com>
2023-06-14 10:36:56 +0200

    change deployment URL

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === "pages" ? "/batijs.github.io/" : undefined,
   plugins: [
     tsconfigPaths(),
     solid({
