fce86a95343a50c78494828e9697cc3e356b22ea
Joël Charles <joel.charles91@gmail.com>
2023-06-15 12:28:03 +0200

    header + footer

--- layouts/LayoutDefault.tsx
+++ layouts/LayoutDefault.tsx
@@ -1,5 +1,4 @@
 import "./tailwind.css";
-import logoUrl from "../assets/logo.svg";
 import type { JSX } from "solid-js";
 import { StoreProvider } from "components/Store";
 
@@ -8,23 +7,5 @@ export default function LayoutDefault(props: { children?: JSX.Element }) {
     <StoreProvider>
       <div class="flex flex-col mx-auto">{props.children}</div>
     </StoreProvider>
-  );
-}
-
-function Logo() {
-  return (
-    <div class="p-5 mb-2">
-      <a href="/">
-        <img alt="Bâti logo" src={logoUrl} height={128} width={128} />
-      </a>
-    </div>
-  );
-}
-
-function Attributions() {
-  return (
-    <a href="https://www.flaticon.com/free-icons/wall" title="wall icons">
-      Wall icons created by Freepik - Flaticon
-    </a>
   );
 }
