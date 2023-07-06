a4d4b74062d7c66e5e1ebb0a3b00c22ef9be2090
Joël Charles <joel.charles91@gmail.com>
2023-07-06 16:29:38 +0200

    build web component with vite

--- pages/index/Index.element.ts
+++ pages/index/Index.element.ts
@@ -2,8 +2,3 @@ import { customElement } from "solid-element";
 import AppWidget from "./AppWidget";
 
 customElement("bati-widget", { theme: "" }, AppWidget);
-if (import.meta.hot) {
-  import.meta.hot.on("force-reload", (data) => {
-    console.log("GUY");
-  });
-}
