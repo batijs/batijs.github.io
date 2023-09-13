9f83bedf69c75ca12400d59177848a858badfd42
Joël Charles <joel.charles91@gmail.com>
2023-09-13 13:42:32 +0200

    feat: adding og meta tags

--- layouts/Head.tsx
+++ layouts/Head.tsx
@@ -7,6 +7,17 @@ export default function Head() {
       />
       <meta http-equiv="X-UA-Compatible" content="ie=edge" />
       <link rel="icon" href="assets/logo.svg" />
+      <meta property="og:title" content="Bati" />
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:image"
+        content="https://raw.githubusercontent.com/batijs/bati/main/doc/bati.png"
+      />
+      <meta property="og:url" content="https://batijs.github.io" />
+      <meta
+        property="og:description"
+        content="Next-gen scaffolder. Get started with fully-functional apps, and choose any tool you want."
+      />
     </>
   );
 }
